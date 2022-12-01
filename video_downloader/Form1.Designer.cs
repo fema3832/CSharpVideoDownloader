5802bf1876101d0627c4a874839d9c8aecc05f71
fema3832 <fema3832@gmail.com>
2022-12-01 17:15:38 +0100

    main

---
+++ video_downloader/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace video_downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.linkBox = new System.Windows.Forms.TextBox();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.formatList = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.outputButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // linkBox
+            // 
+            this.linkBox.Location = new System.Drawing.Point(66, 12);
+            this.linkBox.Name = "linkBox";
+            this.linkBox.Size = new System.Drawing.Size(396, 23);
+            this.linkBox.TabIndex = 0;
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Location = new System.Drawing.Point(31, 41);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(75, 23);
+            this.downloadButton.TabIndex = 1;
+            this.downloadButton.Text = "Download";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
+            // 
+            // formatList
+            // 
+            this.formatList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.formatList.FormattingEnabled = true;
+            this.formatList.Location = new System.Drawing.Point(341, 42);
+            this.formatList.Name = "formatList";
+            this.formatList.Size = new System.Drawing.Size(121, 23);
+            this.formatList.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(290, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Format";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Link";
+            // 
+            // outputButton
+            // 
+            this.outputButton.Location = new System.Drawing.Point(31, 70);
+            this.outputButton.Name = "outputButton";
+            this.outputButton.Size = new System.Drawing.Size(75, 23);
+            this.outputButton.TabIndex = 5;
+            this.outputButton.Text = "Output";
+            this.outputButton.UseVisualStyleBackColor = true;
+            this.outputButton.Click += new System.EventHandler(this.outputButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(112, 74);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(46, 15);
+            this.outputLabel.TabIndex = 6;
+            this.outputLabel.Text = "output:";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(341, 70);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(121, 23);
+            this.saveButton.TabIndex = 7;
+            this.saveButton.Text = "Save Options";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 106);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.outputButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.formatList);
+            this.Controls.Add(this.downloadButton);
+            this.Controls.Add(this.linkBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Video Downloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox linkBox;
+        private Button downloadButton;
+        private ComboBox formatList;
+        private Label label1;
+        private Label label2;
+        private Button outputButton;
+        private Label outputLabel;
+        private Button saveButton;
+    }
+}
---
+++ video_downloader/Form1.cs
@@ -0,0 +1,94 @@
+using VideoLibrary;
+using MediaToolkit;
+using MediaToolkit.Model;
+
+namespace video_downloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string direcotryPath = "";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            formatList.Items.Add("MP4 - VIDEO");
+            formatList.Items.Add("MP3 - AUDIO");
+
+            if (Properties.Settings.Default.isSaved == 1)
+            {
+                formatList.SelectedIndex = Properties.Settings.Default.savedFormat;
+                direcotryPath = Properties.Settings.Default.savedOutput;
+            }
+            else
+            {
+                formatList.SelectedIndex = 0;
+            }
+        }
+
+        private void outputButton_Click(object sender, EventArgs e)
+        {
+            using var fbd = new FolderBrowserDialog();
+            var result = fbd.ShowDialog();
+
+            if (result != DialogResult.OK || string.IsNullOrWhiteSpace(fbd.SelectedPath)) return;
+            direcotryPath = fbd.SelectedPath;
+            outputLabel.Text = direcotryPath;
+        }
+
+        private void downloadButton_Click(object sender, EventArgs e)
+        {
+            if (formatList.SelectedItem == null)
+            {
+                MessageBox.Show("Nincs kiválasztva formátum");
+                return;
+            }
+            if (direcotryPath == "")
+            {
+                MessageBox.Show("Nincs kiválasztva output (célkönyvtár)");
+                return;
+            }
+            if (linkBox.Text == "")
+            {
+                MessageBox.Show("Nincs megadva link");
+                return;
+            }
+
+            try
+            {
+                var youTube = YouTube.Default;
+                var video = youTube.GetVideo(linkBox.Text);
+                var inputFile = new MediaFile { Filename = direcotryPath + @"\" + video.FullName };
+                var outputFile = new MediaFile { Filename = direcotryPath + @"\" + video.FullName.Remove(video.FullName.Length - 4) + ".mp3" };
+
+                if (formatList.SelectedIndex == 1)
+                {
+                    File.WriteAllBytes(direcotryPath + @"\" + video.FullName + " (video)", video.GetBytes());
+                    using var engine = new Engine();
+                    engine.Convert(inputFile, outputFile);
+                    File.Delete(direcotryPath + @"\" + video.FullName);
+                }
+                else
+                {
+                    File.WriteAllBytes(direcotryPath + @"\" + video.FullName, video.GetBytes());
+                }
+
+                MessageBox.Show("Done");
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show("Nem jó a link");
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.savedFormat = formatList.SelectedIndex;
+            Properties.Settings.Default.savedOutput = direcotryPath;
+            Properties.Settings.Default.isSaved = 1;
+        }
+    }
+}
